c2367a7ee0aee5b1e82d28ff832674bef7dce841
Nicowski25 <nicola.faedo0610@gmail.com>
2023-04-19 15:43:29 +0200

    created components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
-<template>
-  <div>
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
-  </div>
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
+</script>
+
+<template>
+
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+  <AppFooter></AppFooter>
+
 </template>
 
-<script>
-  export default {
-    
-  }
-</script>
 
 <style lang="scss" scoped>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        name: 'AppHeader'
+    }
+</script>
+<template>
+    <div class="container text-center">
+        <h1>BOOLFLIX</h1>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
